c784d52053371175e6242f9e008aa9003a71ed22
Trungna5git <87321014+Trungna5git@users.noreply.github.com>
2022-03-31 16:15:37 +0700

    commit
    commit

---
+++ functions/favorite job/drop column.sql
@@ -0,0 +1,6 @@
+ALTER TABLE t_user_favorite_job DROP COLUMN sort;
+ALTER TABLE t_user_favorite_job DROP COLUMN delete_flag;
+ALTER TABLE t_user_favorite_job DROP COLUMN registered_id;
+ALTER TABLE t_user_favorite_job DROP COLUMN registered_datetime;
+ALTER TABLE t_user_favorite_job DROP COLUMN changed_id;
+ALTER TABLE t_user_favorite_job DROP COLUMN changed_datetime;
